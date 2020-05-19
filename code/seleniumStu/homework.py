1070c78b1dfbe6b31dc2eab48ba80701cbbf454f
Ann <1713717826@qq.com>
2020-05-19 21:26:58 +0800

    update

--- code/seleniumStu/homework.py
+++ code/seleniumStu/homework.py
@@ -1,44 +1,25 @@
-import time
-
 from selenium import webdriver
 
 driver = webdriver.Chrome(r"E:\Program Files\JetBrains\PyCharm 2019.3.3\bin\chromedriver.exe")
+driver.implicitly_wait(20)
 driver.get("https://m.weibo.cn/")
+driver.find_element_by_class_name("m-text-cut").click()
 
-driver.find_element_by_css_selector("#app > div.main-wrap > div.lite-topbar.main-top > div.nav-top > a > aside > label > div").click()
-time.sleep(5)
-driver.find_element_by_xpath('//*[@id="app"]/div[1]/div[1]/div[2]/div/div/div[8]/div/div').click()
-time.sleep(5)
-elements = driver.find_elements_by_class_name('card-list')
-time.sleep(5)
-# hot = r"https://simg.s.weibo.com/20190429_hot.png"
-# new = r"https://simg.s.weibo.com/20190429_new.png"
-# fei = r"https://simg.s.weibo.com/20190429_new.png"
-# recom = r"https://simg.s.weibo.com/20190429_recom.png"
-hotlist = []
-newlist = []
-feilist = []
-recomlist = []
+driver.find_element_by_css_selector('#app > div:nth-child(1) > div:nth-child(1) > div.card.m-panel.card16.m-col-2 > div > div > div:nth-child(8) > div > div > h4').click()
+
+elements = driver.find_elements_by_css_selector('#app > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div > div > div')
+
 for ele in elements:
-    img = ele.find_elements_by_class_name("m-link-icon")
-    if img:
-        imglink = img[0].find_element_by_tag_name("img")
-        src = imglink.get_attribute("src")
-        print(src)
-        print("hot" in src)
-        if "hot" in src:
-            hotlist.append(ele.text)
-        if "new" in src:
-            newlist.append(ele.text)
-        if "fei" in src:
-            feilist.append(ele.text)
-        if "recom" in src:
-            recomlist.append(ele.text)
+    icon = ele.find_elements_by_class_name("m-link-icon")
+    if icon:
+        img = icon[0].find_element_by_tag_name("img")
+        if "hot" in img.get_attribute("src"):
+            print('热    {}'.format(ele.find_element_by_class_name("m-text-cut").text))
+        elif "new" in img.get_attribute("src"):
+            print('新    {}'.format(ele.find_element_by_class_name("m-text-cut").text))
+        elif "fei" in img.get_attribute("src"):
+            print('沸    {}'.format(ele.find_element_by_class_name("m-text-cut").text))
+        elif "recom" in img.get_attribute("src"):
+            print('荐    {}'.format(ele.find_element_by_class_name("m-text-cut").text))
 
-print(hotlist)
-print(newlist)
-print(feilist)
-print(recomlist)
-
-
-
+driver.quit()
